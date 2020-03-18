ea4cf955983f0cf1bfd18a306b72cae536d320b1
Marcos Costa <marcos300896@gmail.com>
2020-03-18 19:21:10 +0100

    iplement della lista con l'uso del service

--- src/app/components/lista-videogiochi/lista-videogiochi.component.ts
+++ src/app/components/lista-videogiochi/lista-videogiochi.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { GameItem } from 'src/app/model/game-item';
+import { GameListServiceService } from 'src/app/services/game-list-service.service';
 
 @Component({
   selector: 'app-lista-videogiochi',
@@ -7,48 +8,13 @@ import { GameItem } from 'src/app/model/game-item';
   styleUrls: ['./lista-videogiochi.component.scss']
 })
 export class ListaVideogiochiComponent implements OnInit {
-  @Output()
-  selectGameId: EventEmitter<number> = new EventEmitter()
-
-  menuItems: GameItem[] = [
-   { id: 1,
-    nome:"fifa20",
-    genere:"gioco sportivo",
-    page:3,
-    description:"simulatore di calcio",
-   rating:5,
-    prezzo:40,
-    selected: true,
-    annoUscita:new Date()
-  },
-  { id: 2,
-    nome:"cod",
-    genere:"fps",
-    page:18,
-    description:"spara tutto",
-   rating:5,
-    prezzo:40,
-    selected: true,
-    annoUscita:new Date()
-  },
-  { id: 3,
-    nome:"SuperMarioBros",
-    genere:"platform",
-    page:3,
-    description:"platform",
-   rating:5,
-    prezzo:40,
-    selected: true,
-    annoUscita:new Date()
+  gameItems: GameItem[];
+  constructor(private listaService: GameListServiceService) {
+    this.gameItems = this.listaService.getListaVideogiochi();
   }
-  
-  ];
-  constructor() { }
 
   ngOnInit(): void {
   }
-  selectGameItem(id: number) {
-    this.selectGameId.emit(id);
-  }
+
 
 }
--- src/app/services/game-list-service.service.ts
+++ src/app/services/game-list-service.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { GameItem } from '../model/game-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameListServiceService {
+  private List: GameItem[]= [
+    { id: 1,
+     nome:"fifa20",
+     genere:"gioco sportivo",
+     page:3,
+     description:"simulatore di calcio",
+    rating:5,
+     prezzo:40,
+     selected: true,
+     annoUscita:new Date()
+   },
+   { id: 2,
+     nome:"cod",
+     genere:"fps",
+     page:18,
+     description:"spara tutto",
+    rating:5,
+     prezzo:40,
+     selected: true,
+     annoUscita:new Date()
+   },
+   { id: 3,
+     nome:"SuperMarioBros",
+     genere:"platform",
+     page:3,
+     description:"platform",
+    rating:5,
+     prezzo:40,
+     selected: true,
+     annoUscita:new Date()
+   }
+  ];
 
+  getListaVideogiochi()
+  {
+    return this.List;
+  }
   constructor() { }
-}
+}//TODO: da finire 
