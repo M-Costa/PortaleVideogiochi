53f2616b8d33602a15016e8c98505afe5ecd8d05
Marcos Costa <marcos300896@gmail.com>
2020-03-20 14:13:09 +0100

    conversione nav menu con ilrouter outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,6 +8,13 @@ import { ListaVideogiochiComponent } from './components/lista-videogiochi/lista-
 import { DettaglioVideogiocoComponent } from './components/dettaglio-videogioco/dettaglio-videogioco.component';
 import { ModificaListaComponent } from './components/modifica-lista/modifica-lista.component';
 import { MenuComponent } from './components/menu/menu.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'gameList',      component: ListaVideogiochiComponent },
+  { path: 'modificaLista',      component: ModificaListaComponent },
+  { path: 'dettaglioVideogioco',      component: DettaglioVideogiocoComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +26,11 @@ import { MenuComponent } from './components/menu/menu.component';
     MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,26 +8,8 @@ import { MenuItem } from 'src/app/model/menu-item';
 })
 export class MenuComponent implements OnInit {
 
-  @Output()
-  selectMenuId: EventEmitter<number> = new EventEmitter()
-
-  menuItems: MenuItem[] = [
-    {
-      id: 1, description: 'Home', selected: true
-    },
-    {
-      id: 2, description: 'Lista Videogiochi', selected: true
-    },
-    {
-      id: 3, description: 'Modifica', selected: true
-    }
-  ];
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-  selectMenuItem(id: number) {
-    this.selectMenuId.emit(id);
   }
 }
